9d1ebb8f5c39dc6dd063c9634323401484ca789d
Aborgen <alexanderborgen@gmail.com>
2019-08-27 11:44:16 -0700

    Split hex2b64 into two methods, using immutable bytes instead of bytearray

--- src/modules/utils/convert.py
+++ src/modules/utils/convert.py
@@ -1,6 +1,21 @@
 import base64
 
 def hex2b64(hexString):
-  byteArr = bytearray.fromhex(hexString)
-  return base64.b64encode(byteArr)
+  b = hex2bytes(hexString)
+  return base64.b64encode(b)
 
+def hex2bytes(hexString):
+  isHex = validateHex(hexString)
+  if not isHex:
+    raise ValueError("'${hexString}' is not valid hex")
+
+  b = bytes.fromhex(hexString)
+  return b
+
+def validateHex(hexString):
+  try:
+    int(hexString, 16)
+  except ValueError as err:
+    return false
+
+  return len(hexString) % 2 == 0
