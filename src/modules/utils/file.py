cd1c5d147fe3bd7f6e812a12fa0a4719ad2d794e
Aborgen <alexanderborgen@gmail.com>
2019-08-29 14:03:48 -0700

    Create method for parsing text files

--- src/modules/utils/file.py
+++ src/modules/utils/file.py
@@ -6,3 +6,16 @@ def readJSON(filename):
     data = json.load(jsonData)
 
   return data
+
+def readFile(filename):
+  lines = []
+  with open(filename) as f:
+    for line in f:
+      # It is a comment, ignore it.
+      if line.startswith('#'):
+        continue
+
+    lines.append(line)
+
+  # Remove unnecessary whitespace
+  return [line.rstrip() for line in lines]
