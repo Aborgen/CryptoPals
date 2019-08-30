4068337225ad6dc8c8b02bcb5fad4bbce6da27bc
Aborgen <alexanderborgen@gmail.com>
2019-08-30 16:16:29 -0700

    Add two new arguments that can be used for challenges

--- src/challenges/initChallenge.py
+++ src/challenges/initChallenge.py
@@ -7,6 +7,8 @@ from modules.utils.file import readJSON
 def getArgs():
   parser = argparse.ArgumentParser('Each challenege is set up to run on it\'s own, using value(s) located in data.json. This can be changed by using the --input flag')
   parser.add_argument('-i', '--input', nargs = '*', help = 'The text to be processed. Accepts 0 to many inputs.', type = str)
+  parser.add_argument('-d', '--dictionaryFile', help = 'For challenges that need the contects of some file to complete', type = str)
+  parser.add_argument('-c', '--candidates', help = 'For challenges that need to guess, output information for n of the best guesses', type = int, default = 1)
   args = parser.parse_args()
   return args
 
