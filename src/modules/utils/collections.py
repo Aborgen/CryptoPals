59a84cc60a30bf65ecb8320da2fdccb6535a6fc7
Aborgen <alexanderborgen@gmail.com>
2019-09-03 16:14:42 -0700

    Update logic in normalizeListSize to be clearer

--- src/modules/utils/collections.py
+++ src/modules/utils/collections.py
@@ -1,11 +1,10 @@
-
 # If the provided lists are not the same length,
 # the longer will be truncated to match the shorter.
 def normalizeListSize(aList, bList):
-  if len(aList) == len(bList):
+  aLength, bLength = len(aList), len(bList)
+  if aLength == bLength:
     return
-  # Ensure that aList is the longer of the two lists
-  if len(aList) < len(bList):
-    aList, bList = bList, aList
-
-  aList = aList[0:len(bList) - 1]
+  elif aLength > bLength:
+    del aList[bLength - 1:]
+  else:
+    del bList[aLength - 1:]
