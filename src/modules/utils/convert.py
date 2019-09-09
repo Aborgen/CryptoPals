8d744af75851ad4b8565c9a89d68a66887a85b51
Aborgen <alexanderborgen@gmail.com>
2019-09-09 15:53:12 -0700

    Add functions that convert between bytes and base64 encoded strings

--- src/modules/utils/convert.py
+++ src/modules/utils/convert.py
@@ -1,14 +1,27 @@
 import base64
 from .compare import _validateBytes, _validateHex, isHex
 
+# Base64 conversion
 def hex2b64(hexString):
   b = hex2bytes(hexString)
-  return base64.b64encode(b)
+  return bytes2b64(b)
 
 def b642hex(b64String):
-  b = base64.b64decode(b64String, validate = True)
+  b = b642bytes(b64String)
   return bytes2hex(b)
 
+def bytes2b64(b):
+  isBytes = _validateBytes(b)
+  if not isBytes:
+    raise ValueError(f"'{b}' is not a bytes object")
+
+  return base64.b64encode(b)
+
+def b642bytes(b64String):
+  b = base64.b64decode(b64String, validate = True)
+  return b
+
+# Byte conversion
 def hex2bytes(hexString):
   isHex, eInfo = _validateHex(hexString)
   if not isHex:
