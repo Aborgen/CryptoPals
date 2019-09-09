0344fe8c44c261395056876cf0854b56c71c9b03
Aborgen <alexanderborgen@gmail.com>
2019-09-09 15:55:27 -0700

    Create function wrapping around PyCryptdome's AES object. Ability to encrypt as well as decrypt

--- src/modules/crypto/crypto.py
+++ src/modules/crypto/crypto.py
@@ -1,7 +1,14 @@
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+from enum import Enum
 from . import initCrypto
 from ..utils.collections import normalizeListSize
 from ..utils.compare import isBytes
 from ..utils.convert import toBytes
+
+class Action(Enum):
+  ENCRYPT = 0
+  DECRYPT = 1
 
 def fixedXOR(a, b):
   if not isBytes(a):
@@ -18,3 +25,22 @@ def fixedXOR(a, b):
 
 def repeatingXOR(line, key):
   return fixedXOR(line, key * len(line))
+
+def ECB_AES128(key, text, action):
+  if not isBytes(key):
+    key = toBytes(key)
+
+  if not isBytes(text):
+    text = toBytes(text)
+
+  cipher = AES.new(key, AES.MODE_ECB)
+
+  if action == Action.ENCRYPT:
+    # ECB requires length to be a multiple of 16.
+    text = pad(text, 16)
+    return cipher.encrypt(text)
+  elif action == Action.DECRYPT:
+    return cipher.decrypt(text)
+  else:
+    raise ValueError
+
