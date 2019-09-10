485496fb62834195cd3d9154ff639fb40eb4bbf2
Aborgen <alexanderborgen@gmail.com>
2019-09-10 16:47:04 -0700

    Remove unused parameter from pad

--- src/modules/utils/modify.py
+++ src/modules/utils/modify.py
@@ -1,15 +1,15 @@
 from .compare import isBytes
 from .convert import toBytes
 
-def pad(text, length, symbol):
+def pad(text, length):
   if len(text) % length == 0:
     return text
 
-  paddedText = _padBytes(text)
+  paddedText = _padBytes(text, length)
   return paddedText if isBytes(text) else paddedText.decode()
 
 # This is an implementation of PKCS#7 padding
-def _padBytes(text, length, byte):
+def _padBytes(text, length):
   if not isBytes(text):
     text = toBytes(text)
 
