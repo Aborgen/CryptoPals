2e59cde626ad6aebd74feb7db29c6cfd02f2c6b4
Aborgen <alexanderborgen@gmail.com>
2019-09-11 10:06:36 -0700

    Remove leading newline

--- src/modules/utils/compare.py
+++ src/modules/utils/compare.py
@@ -1,4 +1,3 @@
-
 # We are interested in knowing--when we compare the two bytes objects, byte for byte--how many
 # bits are different from each other. After XORing two bytes, counting all the ones will give us this answer.
 def hammingDistance(aBytes, bBytes):
