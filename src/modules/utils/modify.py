e944a30bcddc30b8eb94c1017375c4fa88c5a2d0
Aborgen <alexanderborgen@gmail.com>
2019-09-11 14:47:38 -0700

    Create function that repeats a given string (str or byte) character by character until it reaches a specified length

--- src/modules/utils/modify.py
+++ src/modules/utils/modify.py
@@ -1,3 +1,4 @@
+from itertools import cycle
 from .compare import isBytes
 from .convert import toBytes
 
@@ -17,3 +18,11 @@ def _padBytes(text, length):
   correctedLength = len(text) + missing
   return text.ljust(correctedLength, bytes([missing]))
 
+# Text will be repeated until its length matches the provided length
+# text = 'text', length = 10
+# 'texttextte'
+def repeatPerCharacter(text, length):
+  c = cycle(text)
+  repeatText = [next(c) for _ in range(length)]
+
+  return bytes(repeatText) if isBytes(text) else ''.join(repeatText)
